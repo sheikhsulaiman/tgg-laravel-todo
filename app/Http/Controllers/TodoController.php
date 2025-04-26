d94b079b6308f8a1fa08be01eed757d7daecb6ac
sheikhsulaiman <sheikhsulaimansony@outlook.com>
2025-04-26 06:31:52 +0600

    Build the Controller Logic

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
+
 
 class TodoController extends Controller
 {
-    //
+    public function index() {
+        $todos = Todo::all();
+        return view('todos.index', compact('todos'));
+    }
+
+    public function store(Request $request) {
+        $request->validate(['title' => 'required']);
+        Todo::create($request->only('title'));
+        return redirect('/');
+    }
+
+    public function update(Request $request, $id) {
+        $todo = Todo::findOrFail($id);
+        $todo->update(['completed' => !$todo->completed]);
+        return redirect('/');
+    }
+
+    public function destroy($id) {
+        Todo::destroy($id);
+        return redirect('/');
+    }
 }
